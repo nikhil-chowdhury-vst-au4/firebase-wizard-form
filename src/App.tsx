4147055046ffa316edd9e315b512043827736558
nikhil-chowdhury-vst-au4 <nikhilchowdhury666@gmail.com>
2021-02-24 20:07:21 +0530

    my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Form from "../src/Components/form"
+import Question from "../src/Components/Header/header"
+import FormPage from "../src/Components/forms/Login"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Question />
+   <FormPage />
     </div>
   );
 }
